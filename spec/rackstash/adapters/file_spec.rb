724a373d665b8f532eb219cb54f7f78fa6f5da39
Holger Just <hello@holgerjust.de>
2017-07-15 14:29:02 +0200

    Add MIT license header to file adapter spec

--- spec/rackstash/adapters/file_spec.rb
+++ spec/rackstash/adapters/file_spec.rb
@@ -1,3 +1,8 @@
+# Copyright 2017 Holger Just
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE.txt file for details.
+
 require 'spec_helper'
 require 'tempfile'
 require 'tmpdir'
