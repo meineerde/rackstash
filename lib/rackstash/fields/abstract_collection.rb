1687a817b7fade3a6debcfdc9a8f9655c11429b5
Holger Just <hello@holgerjust.de>
2018-01-24 20:22:34 +0100

    Do not require 'complex'. It's not needed and deprecated

--- lib/rackstash/fields/abstract_collection.rb
+++ lib/rackstash/fields/abstract_collection.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 #
-# Copyright 2017 Holger Just
+# Copyright 2017 - 2018 Holger Just
 #
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE.txt file for details.
 
 require 'bigdecimal'
-require 'complex'
 require 'pathname'
 require 'uri'
 
