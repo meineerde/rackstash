08ca86b7c32a720c97f98c3c210a1109cb313fab
Holger Just <hello@holgerjust.de>
2020-07-10 18:47:58 +0200

    Explicitly require rack/mock in rack middleware spec

--- spec/rackstash/rack/middleware_spec.rb
+++ spec/rackstash/rack/middleware_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 #
-# Copyright 2017 - 2018 Holger Just
+# Copyright 2017 - 2020 Holger Just
 #
 # This software may be modified and distributed under the terms
 # of the MIT license. See the LICENSE.txt file for details.
 
 require 'spec_helper'
+require 'rack/mock'
 
 require 'rackstash/rack/middleware'
 
@@ -253,7 +254,7 @@ RSpec.describe Rackstash::Rack::Middleware do
       expect(log.last).to include(
         'error' => 'RuntimeError',
         'error_message' => 'Oh noes!',
-        'error_trace' => %r{\A#{__FILE__}:24:in}
+        'error_trace' => %r{\A#{__FILE__}:25:in}
       )
     end
 
