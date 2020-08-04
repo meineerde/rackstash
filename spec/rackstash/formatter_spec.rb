bb78d986397c11939036793c17b04582af1d3da0
Holger Just <hello@holgerjust.de>
2020-08-04 18:07:42 +0200

    Remove unused RawFormatter

--- spec/rackstash/formatter_spec.rb
+++ spec/rackstash/formatter_spec.rb
@@ -58,19 +58,3 @@ RSpec.describe Rackstash::Formatter do
       .and be_frozen
   end
 end
-
-RSpec.describe Rackstash::RawFormatter do
-  let(:formatter) { described_class.new }
-
-  it 'returns the message' do
-    msg = 'my message'
-    expect(formatter.call('ERROR', Time.now, 'progname', msg)).to equal msg
-  end
-
-  it 'inspects non-string messages' do
-    obj = Object.new
-
-    expect(obj).to receive(:inspect).and_return('object')
-    expect(formatter.call('ERROR', Time.now, 'progname', obj)).to eql 'object'
-  end
-end
